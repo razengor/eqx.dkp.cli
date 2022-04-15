c7128bb668b773eafdf21a2e658cbc7be03f9463
Razengor <irachulito@gmail.com>
2022-04-16 00:23:00 +0200

    Botón para ocultar grupos de fichas funcional

--- src/components/CharacterGroupName.vue
+++ src/components/CharacterGroupName.vue
@@ -1,10 +1,11 @@
 <template>
   <section>
-    <span :data-filter="group.gName.toLowerCase()">
+    <span :data-filter="group.gName.toLowerCase()" @click="checkGroup(group.gName.toLowerCase())">
       {{group.gName}}
-      <img src="../assets/img/ico_arrow-down.png">
+      <img v-if="hiddenGroups[group.gName.toLowerCase()]" src="../assets/img/ico_arrow-up.png">
+      <img v-if="!hiddenGroups[group.gName.toLowerCase()]" src="../assets/img/ico_arrow-down.png">
     </span>
-    <div :id="'grupo-'+group.gName.toLowerCase()" class="char-group">
+    <div :id="'grupo-'+group.gName.toLowerCase()" class="char-group" v-show="!hiddenGroups[group.gName.toLowerCase()]">
       <character-card v-for="(char, index) in group.characters" :key="index" :game="game" :charId="index" :char="char" @editChar="transmitEvent" />
     </div>
   </section>
@@ -20,11 +21,16 @@ export default {
     "character-card": CharacterCard
   },
   data () {
-    return {}
+    return {
+      hiddenGroups: []
+    }
   },
   methods: {
     transmitEvent(holder) {
       this.$emit('editChar',holder);
+    },
+    checkGroup(gName) {
+      this.hiddenGroups[gName] = this.hiddenGroups[gName]?false:true;
     }
   }
 }
