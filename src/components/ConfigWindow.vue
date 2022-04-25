9feab39268bcba7aa3c7a03ad489e716592790e0
Razengor <irachulito@gmail.com>
2022-04-26 01:19:49 +0200

    Ventana de Configuración de Partida

--- src/components/ConfigWindow.vue
+++ src/components/ConfigWindow.vue
@@ -1,10 +1,23 @@
 <template>
     <div id="window-conf" class="window shown">
         <h3>CONFIGURACIÓN</h3>
-        <form class="info-basic window-box"></form>
+        <div class="info-basic window-box">
+          <div>
+            <p>Nombre de la Partida</p>
+            <input type="text" v-model="editingGame.name">
+            <p>Versión</p>
+            <input type="text" v-model="editingGame.version">
+          </div>
+          <div>
+            <p>Fecha de inicio</p>
+            <input type="text" v-model="editingGame.date">
+            <p>Fecha de la última sesión</p>
+            <input type="text" v-model="editingGame.lastDate">
+          </div>
+        </div>
         <div class="window-footer">
-            <button class="save">GUARDAR</button>
-            <button class="cancel">CANCELAR</button>
+            <button class="save" @click="saveConf">GUARDAR</button>
+            <button class="remove" @click="$emit('removeGame')">ELIMINAR PARTIDA</button>
         </div>
     </div>
 </template>
@@ -12,11 +25,19 @@
 <script>
 export default {
   name: 'ConfigWindow',
-  props: [],
+  props: ["game"],
   data () {
     return {
-
+      editingGame: {}
     }
+  },
+  methods: {
+    saveConf() {
+      this.$emit('saveConf',this.editingGame)
+    }
+  },
+  mounted() {
+    this.editingGame = this.game;
   }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,8 @@
   <div id="home">
     <Header v-if="games[0]" :gameName="getGame(gameid)?getGame(gameid).name:'Default'" :gameVersion="gameVersion" @changeTab="changeTab"/>
 
-    <window-config  v-if="games[0]" v-show="selected=='conf'" />
-    <window-players v-if="games[0]" v-show="selected=='play'" />
+    <window-config  v-if="games[0] && selected=='conf'" :game="game" @saveConf="saveGameConfig" @removeGame="removeGame" />
+    <window-players v-if="games[0] && selected=='play'" />
     <window-char    v-if="games[0] && selected=='char'" :game="game" :gameId="gameid" :isPlayer="false" @editChar="openCharEditWindow" />
     <window-comb    v-if="games[0] && selected=='comb'" />
 
@@ -28,7 +28,9 @@ import {mapActions, mapState} from 'vuex'
 
 import app from '../firebase'
 import { getDatabase, ref, set } from "firebase/database";
+import router from '../router/index'
 
+const db = getDatabase(app);
 
 export default {
   name: 'HomeView',
@@ -88,13 +90,33 @@ export default {
       document.getElementById("body").classList.remove('editing-char');
     },
     saveCharToEdit(holder) {
-      const db = getDatabase(app);
+      
       set(ref(db, "games/"+this.game.id+"/characters/"+holder.id), holder.charToEdit);
       //this.closeCharWindow();
       window.alert("¡Se han guardado los datos!");
     },
     deleteChar() {
       window.alert("La función de eliminado aun está en proceso. Si quiere eliminar información contacte con el administrador...");
+    },
+    saveGameConfig(gameInfo) {
+      if(gameInfo!=null) {
+        if(gameInfo.name!=null && gameInfo.name.length <= 20 && gameInfo.name.length >= 3)
+          set(ref(db, "games/"+this.game.id+"/name"), gameInfo.name);
+        if(gameInfo.version!=null && gameInfo.version.length <= 5 && gameInfo.version.length >= 1)
+          set(ref(db, "games/"+this.game.id+"/version"), gameInfo.version);
+        if(gameInfo.date!=null && gameInfo.date.length <= 10 && gameInfo.date.length >= 8)
+          set(ref(db, "games/"+this.game.id+"/date"), gameInfo.date);
+        if(gameInfo.lastDate!=null && gameInfo.lastDate.length <= 10 && gameInfo.lastDate.length >= 8)
+          set(ref(db, "games/"+this.game.id+"/lastDate"), gameInfo.lastDate);
+      }
+    },
+    removeGame() {
+      if(confirm("¿Estás seguro de que quieres eliminar TODA la partida '"+this.gameName+"'?")) {
+        if(confirm("¿¡SEGURO!?")) {
+          set(ref(db, "games/"+this.game.id), {});
+          router.push('/index');
+        }
+      }
     }
   },
   computed: {
